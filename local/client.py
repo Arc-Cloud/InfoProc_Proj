ff161144b494d2e56e38e17514898a22b35acc8b
lucasng32 <lucasng3.2@gmail.com>
2024-03-02 12:06:43 +0000

    snake game on server test

--- local/client.py
+++ local/client.py
@@ -3,35 +3,128 @@ import time
 import accelerometer_input as acc
 import json
 import os
+import pygame, random
 
-class client:
-    #the server name and port client wishes to access
-    server_name = '13.60.28.247'
-    server_port = 12000
-    #create a UDP client socket
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+#the server name and port client wishes to access
+server_name = '13.48.57.52'
+server_port = 12000
+#create a UDP client socket
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-    # client_socket.bind(('localhost', 10000))
+# client_socket.bind(('localhost', 10000))
 
-    print("UDP client running...")
-    print("Connecting to server at IP: ", server_name, " PORT: ", server_port)
+print("UDP client running...")
+print("Connecting to server at IP: ", server_name, " PORT: ", server_port)
+
+# initialize game
+pygame.init()
+pygame.font.init()
+
+SCREEN_X= 720
+"Width of the screen"
+SCREEN_Y=480
+"Height of the screen"
+
+pygame.display.set_caption('Sophie Snake Slay')
+window = pygame.display.set_mode((SCREEN_X, SCREEN_Y))
+"Target window for game"
+
+clock = pygame.time.Clock()
+
+SNAKE_COLOUR = (255, 0, 247)
+FOOD_COLOUR = (0, 255, 166)
+SCORE_COLOUR = (255, 0, 247)
+
+class Coord():
+    "X and Y coordinates"
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+FOOD_WIDTH = 20
+def generateFood():
+    return pygame.Rect(random.randint(0, SCREEN_X - FOOD_WIDTH),
+                       random.randint(0, SCREEN_Y - FOOD_WIDTH),
+                       FOOD_WIDTH, FOOD_WIDTH)
+
+SNAKE_WIDTH = 20
+score = 0
 
 
-    while True:
-        msg = {
-            'x' : acc.Input.getX(),
-            'y' : acc.Input.getY()
-        }
+sounds = { "omnom" : pygame.mixer.Sound("eat_sound.mp3"),
+           "oopsydaisy" : pygame.mixer.Sound("collision_sound.mp3"),
+           "womp_womp" : pygame.mixer.Sound("womp-womp.mp3")
+         }
 
-        msg_json = json.dumps(msg)
+def gamover():
+    my_font = pygame.font.SysFont('Times New Roman', 90)
+    game_over_surface = my_font.render('YOU DIED', True, (252,3,3) )
+    game_over_rect = game_over_surface.get_rect()
+    game_over_rect.midtop = (SCREEN_X/2, SCREEN_Y/4)
+    window.fill((10,10,10))
+    window.blit(game_over_surface, game_over_rect)
+    show_score(0, (252,3,3), 'Comic Sans', 20)
+    pygame.display.flip()
+    sounds["womp_womp"].play()
+    time.sleep(3)
+    pygame.quit()
+
+def show_score(choice, colour, font, size):
+    score_font = pygame.font.SysFont(font, size)
+    score_surface = score_font.render('Score : ' + str(score), True, colour)
+    score_rect = score_surface.get_rect()
+    if choice == 1:
+        score_rect.midtop = (SCREEN_X/10, 15)
+    else:
+        score_rect.midtop = (SCREEN_X/2, SCREEN_Y/1.25)
+    window.blit(score_surface, score_rect)
+
+while True:
+    msg = {
+        'x' : acc.Input.getX(),
+        'y' : acc.Input.getY()
+    }
+
+    msg_json = json.dumps(msg)
         
-        #send the message to the udp server
-        client_socket.sendto(msg_json.encode(),(server_name, server_port))
+    #send the message to the udp server
+    client_socket.sendto(msg_json.encode(),(server_name, server_port))
 
-        #return values from the server
-        msg, sadd = client_socket.recvfrom(2048)
-        #show output and close client
-        os.system('cls')
-        print(json.loads(msg))
-        time.sleep(0.01)
+    #return values from the server
+    msg_ret, sadd = client_socket.recvfrom(2048)
+
+    #process gameData here
+    gameData = json.loads(msg_ret)
+
+    food = gameData['food']
+
+    users = gameData['userlist']
+
+    score = list(users.keys())[0]['score']
+
+    #rendering
+    window.fill('black')
+    for user in users:
+        for pos in users[user]['body']:
+            pygame.draw.rect(window, SNAKE_COLOUR, pygame.Rect(pos.x, pos.y, SNAKE_WIDTH, SNAKE_WIDTH))
+
+    pygame.draw.rect(window, FOOD_COLOUR, food)
+
+    #game over
+    if gamover:
+        sounds["oopsydaisy"].play()
+        gamover()
+
+    #refresh
+    show_score(1, SCORE_COLOUR, 'Comic Sans', 20)
+    pygame.display.update()
+
+    # limits FPS to 60
+    clock.tick(60)
+
+    #show output and close client
+    #os.system('cls')
+    #print(json.loads(msg))
+    #time.sleep(0.01)
 
--- local/server.py
+++ local/server.py
@@ -1,5 +1,7 @@
 import socket
 import json
+import pygame
+import random
 
 #select a server port
 server_port = 12000
@@ -8,7 +10,7 @@ server_port = 12000
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 #bind the server to the localhost at port server_port
-server_socket.bind(('172.31.32.91',server_port))
+server_socket.bind(('172.31.47.170',server_port))
 
 print('UDP Server running on port ', server_port)
 
@@ -16,6 +18,46 @@ print('UDP Server running on port ', server_port)
 #As UDP is not connection oriented,the same UDP socket serves all clients
 
 userList = {}
+
+# game initialization stuff here
+
+pygame.init()
+
+SCREEN_X= 720
+"Width of the screen"
+SCREEN_Y=480
+"Height of the screen"
+
+clock = pygame.time.Clock()
+
+FOOD_WIDTH = 20
+
+class Coord():
+    "X and Y coordinates"
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+def generateFood():
+    return pygame.Rect(random.randint(0, SCREEN_X - FOOD_WIDTH),
+                       random.randint(0, SCREEN_Y - FOOD_WIDTH),
+                       FOOD_WIDTH, FOOD_WIDTH)
+
+SNAKE_WIDTH = 20
+
+accurate_x = 100.0
+accurate_y = 50.0
+
+gameover = False
+
+snake_pos = pygame.Rect(accurate_x, accurate_y, SNAKE_WIDTH, SNAKE_WIDTH)
+"Head of the snake"
+
+snake_body = [Coord(snake_pos.x, snake_pos.y)]
+"'Units' of the snake body"
+
+food = generateFood()
 
 while True:
 
@@ -28,19 +70,38 @@ while True:
 
     #If first time user, add initial position (spawn snake head)
     if cadd[0] not in userList:
-        userList[cadd[0]] = {'x': 0, 'y': 0}
+        userList[cadd[0]] = {'position': snake_pos, 'body': snake_body, 'score' : 0}
         
     #Update snake head position
-    userList[cadd[0]]['x'] -= msg_x * 10
-    userList[cadd[0]]['y'] += msg_y * 10
-    msg = json.dumps(userList)
+    userList[cadd[0]]['position'].x -= msg_x * 10
+    userList[cadd[0]]['position'].y += msg_y * 10
+    
+    #update snake body
+    userList[cadd[0]]['body'].insert(0, Coord(snake_pos.x, snake_pos.y))
+
+    # snake be getting bigger
+    for i in userList: 
+        if userList[i]['position'].colliderect(food):
+            food = generateFood()
+            userList[i]['score'] += 1
+            break
+        else:
+            userList[i]['body'].pop()
+    
+    if snake_pos.x < 0 or snake_pos.x > SCREEN_X-SNAKE_WIDTH or snake_pos.y < 0 or snake_pos.y > SCREEN_Y-SNAKE_WIDTH:
+        gameover = True
+        pygame.quit()
+
+    gameState = {'userlist': userList, 'food': food, 'gameover': gameover}
+
+    msg = json.dumps(gameState)
     server_socket.sendto(msg.encode(),(cadd[0], cadd[1]))
     #send reply message to all other client process 
     # for i in userList:
     #     if (i != cadd):
     #         server_socket.sendto(msg.encode(),(cadd[0], cadd[1]))
     #         print("Message forwarded to client " + str(cadd))    
-               
+    clock.tick(60)
 
     
 
