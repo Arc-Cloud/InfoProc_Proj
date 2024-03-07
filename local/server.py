279f83f429e2f6fb3e913651378344227d1892ab
lucasng32 <lucasng3.2@gmail.com>
2024-03-07 11:09:58 +0000

    print as a delay

--- local/server.py
+++ local/server.py
@@ -60,18 +60,21 @@ while True:
     msg = json.loads(json_msg)
     msg_x = msg["x"]
     msg_y = msg["y"]
-    
+    print(cadd)
 
     #If first time user, add initial position (spawn snake head)
     if cadd[0] not in userList:
         userList[cadd[0]] = {'position': snake_pos, 'body': snake_body, 'score' : 0}
         print("user joined")
-        print(cadd)
 
+    print("before adding user")    
+    print(userList)
     #Update snake head position
     userList[cadd[0]]['position'][0] -= msg_x * 10
     userList[cadd[0]]['position'][1] += msg_y * 10
 
+    print("update position")
+    print(userList)
     
     #update snake body
     userList[cadd[0]]['body'].insert(0, (snake_pos[0], snake_pos[1]))
@@ -92,7 +95,7 @@ while True:
         gameover = True
         pygame.quit()
     
-
+    print(userList)
 
     food_decode = (food.left, food.top, food.width, food.height)
 
@@ -100,7 +103,7 @@ while True:
 
     msg = json.dumps(gameState)
     server_socket.sendto(msg.encode(),(cadd[0], cadd[1]))
-
+    print(userList)
     #send reply message to all other client process 
     # for i in userList:
     #     if (i != cadd):
