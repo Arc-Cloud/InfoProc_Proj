4aa5edf2ed57df986d098ecc3ac0b473b35477fb
Hanif rais <hanifrais17@gmail.com>
2024-03-09 14:20:12 +0000

    instantiate database and for deleting databse

--- src/database/database.py
+++ src/database/database.py
@@ -104,19 +104,3 @@ def get_player_stats(SessionId, PlayerId, dynamodb=None):
         # Handle the exception appropriately
         return None
 
-
-# for deleting stff duh
-def delete_leaderboard_table(dynamodb=None):
-    if not dynamodb:
-        dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
-
-    table = dynamodb.Table('Leaderboard')
-
-    try:
-        response = table.delete()
-        print("Table deletion initiated successfully.")
-        return response
-    except ClientError as e:
-        print(f"Error deleting table: {e}")
-        # Handle the exception appropriately
-        return None
